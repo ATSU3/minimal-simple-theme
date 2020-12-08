a756b8595c58e5dfd1158263203d5703a78cca85
ATSU <mifutsua@gmail.com>
2020-12-08 16:54:08 +0800

    createBase

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function custom_theme_assets() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
   wp_enqueue_style( 'reset', get_template_directory_uri() . '/css/reset.css', array(), '1.0.0', 'all' );
+  wp_enqueue_style( 'reset', get_template_directory_uri() . '/css/main.css', array(), '1.0.0', 'all' );
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
